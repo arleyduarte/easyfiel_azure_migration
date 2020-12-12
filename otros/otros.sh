f52841ca553b21a4846d408465500d69d9fb9238
Arley Duarte <arleymauricio@gmail.com>
2020-12-12 16:53:02 -0300

    task

--- otros/otros.sh
+++ otros/otros.sh
@@ -1,6 +1,14 @@
 #az login
 
 az group create --name easyfiel_storage_rg --location southcentralus
+az group create --name easyfiel_tasks_rg --location southcentralus
+
+az deployment group create --resource-group easyfiel_tasks_rg --template-file app_service_tasks.json
+
+az deployment group create --resource-group easyfiel_tasks_rg --template-file app_service_tasks.json
+
+az deployment group create --resource-group easyfiel_tasks_rg --template-file web_app.json
+
 
 az deployment group create --resource-group easyfiel_storage_rg --template-file storage.json
 
